198f0c03bf2553b98345e6379afb3ede9286285e
nghia.huynht05 <nghia.huynht05@gmail.com>
2020-12-21 14:07:22 +0700

    Feat(Blog) Handler error

--- node-server/model/blogModel.js
+++ node-server/model/blogModel.js
@@ -1,24 +1,29 @@
-const { fromCallback } = require("bluebird");
+const { fromCallback, resolve } = require("bluebird");
 var mongoose = require("mongoose");
+var debug = require('debug');
+var debuglog = debug('BlogModel:*');
+var Promise = require('bluebird');
 
 var Blog = require("../model/schema/blogSchema");
 
-function BlogModel() {}
+function BlogModel() { }
 
-BlogModel.prototype.save = function (callback) {
-  Blog.insertMany({
-    title: "A",
-    author: "Nghia",
-    body: "Hello",
-    hidden: true,
-  })
-    .then(function (res) {
-      debuglog("Inserted", res.length, res.length < 2 ? "blog" : "blogs");
-      callback();
-    })
-    .catch(function (error) {
-      callback(error);
+BlogModel.prototype.save = function () {
+  return new Promise(function (resolve, reject) {
+    Blog.insertMany({
+      title: "A",
+      author: "Nghia",
+      body: "Hello",
+      hidden: true,
+    }, function (error, res) {
+      if (!error) {
+        resolve(res)
+      } else {
+        debuglog("ERROR", "BlogModel.insertMany", "error", null, "import Error: " + error.code);
+        reject(error)
+      }
     });
+  });
 };
 
 module.exports = BlogModel;
