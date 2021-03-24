a889cbe313c8d23de69a6ecbe86e77805e1194c0
nghia.huynht05 <nghia.huynht05@gmail.com>
2021-03-24 11:36:19 +0700

    Feat(API) create scenario request api

---
+++ test/feature/api.feature
@@ -0,0 +1,9 @@
+Feature: Feature name
+
+    Feature Description: Scenario name
+    Background: Background name
+
+    Scenario: Scenario name
+        Given I want to request API "/api/user/add" with params
+            | params                                        |
+            | {"userName":"admin", "email":"email@zing.vn"} |
--- test/feature/step_defintions/common.js
+++ test/feature/step_defintions/common.js
@@ -5,23 +5,42 @@ var debug = require('debug');
 var debuglog = debug('Common:*');
 
 Then(/^I should get the object result matches with$/, function (table, callback) {
-    var self = this;
-    var expectData = JSON.parse(table.hashes()[0].response);
-    debuglog("Expect data ", JSON.stringify(expectData));
-    debuglog("self.storage.resultData ", JSON.stringify(self.storage.resultData));
-    assert.isTrue(self.matchData(self.storage.resultData, expectData), "Result object is not matched with the expected one");
-    self.wsClient.disconnect();
-    callback()
+  var self = this;
+  var expectData = JSON.parse(table.hashes()[0].response);
+  debuglog("Expect data ", JSON.stringify(expectData));
+  debuglog("self.storage.resultData ", JSON.stringify(self.storage.resultData));
+  assert.isTrue(self.matchData(self.storage.resultData, expectData), "Result object is not matched with the expected one");
+  self.wsClient.disconnect();
+  callback()
 });
 
 Then(/^The return data should be matched correctly order with$/, function (table, callback) {
-    if (this.result.error || this.result.errorCode) {
-      callback(this.result)
-    }
-    var self = this;
-    var expectedData = self.parse(table.hashes()[0].response);
-    debuglog("expectedData ", JSON.stringify(expectedData));
-    debuglog("self.result.res ", JSON.stringify(self.result.res));
-    assert.isTrue(self.matchDataWithTheSameOrder(self.result.res, expectedData), "Result object is not matched with the expected one")
-    callback();
-  });
+  if (this.storage.error || this.storage.errorCode) {
+    callback(this.storage)
+  }
+  var self = this;
+  var expectedData = self.parse(table.hashes()[0].response);
+  debuglog("expectedData ", JSON.stringify(expectedData));
+  debuglog("self.storage.res ", JSON.stringify(self.storage.resultData));
+  assert.isTrue(self.matchDataWithTheSameOrder(self.storage.resultData, expectedData), "Result object is not matched with the expected one")
+  callback();
+});
+
+Given(/^I want to request API "([^"]*)" with params$/, function (api, table, callback) {
+  var self = this;
+  var params = table.hashes(0)[0].params;
+  self.urlClient
+    .post(api)
+    .set('content-type', 'application/json')
+    .set('accept', 'application/json')
+    .send(JSON.parse(params))
+    .expect(200)
+    .then(function (response) {
+      debuglog(`- API '${api}' - self.storage.resultData`, JSON.stringify(response.body));
+      self.storage.resultData = response.body;
+      callback();
+    })
+    .catch(function (error) {
+      callback(error);
+    })
+});
