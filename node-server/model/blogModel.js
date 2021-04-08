c1d8c8fac74ed7baafab1e169e4036ae7a8d7aeb
nghia.huynht05 <nghia.huynht05@gmail.com>
2021-04-08 17:19:01 +0700

    Feat(ES2015) update code style ES2015 user and blog

--- node-server/model/blogModel.js
+++ node-server/model/blogModel.js
@@ -4,20 +4,22 @@ var Promise = require('bluebird');
 
 var Blog = require("../model/schema/blogSchema");
 
-function BlogModel() { }
-
-BlogModel.prototype.save = function (parameters) {
-  return new Promise(function (resolve, reject) {
-    parameters = parameters || {};
-    Blog.insertMany(parameters, function (error, res) {
-      if (!error) {
-        resolve(res)
-      } else {
-        debuglog("ERROR", "BlogModel.insertMany", "error", null, "import Error: " + error.code);
-        reject(error)
-      }
+class BlogModel {
+  constructor() { }
+  save(parameters) {
+    return new Promise(function (resolve, reject) {
+      parameters = parameters || {};
+      Blog.insertMany(parameters, function (error, res) {
+        if (!error) {
+          resolve(res);
+        } else {
+          debuglog("ERROR", "BlogModel.insertMany", "error", null, "import Error: " + error.code);
+          reject(error);
+        }
+      });
     });
-  });
-};
+  }
+}
+
 
 module.exports = BlogModel;
--- node-server/model/userModel.js
+++ node-server/model/userModel.js
@@ -5,68 +5,69 @@ const jwt = require('jsonwebtoken');
 
 const UserSchema = require('./schema/userSchema');
 var ERROR_CODE = require('../config/errorCode');
-function UserModel() { }
-
-UserModel.prototype.createUser = function (parameters) {
-    var select = {
-        userName: 1,
-        firstName: 1,
-    }
-    return new Promise(function (resolve, reject) {
-        UserSchema.insertMany(parameters, function (error, res) {
-            if (!error) {
-                resolve({ userName: res[0].userName, firstName: res[0].firstName, lastName: res[0].lastName })
-            } else {
-                debuglog("ERROR", "UserSchema.insertMany", "error", "import Error: " + error.code);
-                reject(error)
-            }
-        });
-    });
-};
-
-UserModel.prototype.findOne = function (parameters) {
-    return UserSchema.findOne(parameters).then(function (user) {
-        debuglog("DEBUG", "UserSchema.findOne", "user details", JSON.stringify(user));
-        return user;
-    }).catch(function (error) {
-        debuglog("ERROR", "UserSchema.findOne", "error", error);
-        throw error;
-    })
-}
-
-UserModel.prototype.login = function (parameters) {
-    return UserSchema.findOne({ userName: parameters.userName }).then(function (infoUser) {
-        debuglog("DEBUG", "UserSchema.findOne", "user details", JSON.stringify(infoUser));
-        if (!infoUser.isActive) {
-            return Promise.reject({ errorCode: ERROR_CODE.isACTIVE, message: "User is deactive." })
-        }
-        if (infoUser.password !== parameters.password) {
-            return Promise.reject({ errorCode: ERROR_CODE.WORNG_PASSWORD, message: "Password incorrect." })
-        }
-        var token = generateAuthToken(infoUser._id);
-        try {
-            return Promise.all([UserSchema.findOneAndUpdate({ _id: infoUser._id }, { $set: { token: token } })]).spread(function (res) {
-                debuglog("DEBUG", "UserSchema.findOnAndUpdate", "respone", JSON.stringify(res));
-                return UserSchema.findOne({ userName: parameters.userName });
+class UserModel {
+    constructor() { }
+    createUser(parameters) {
+        var select = {
+            userName: 1,
+            firstName: 1,
+        };
+        return new Promise(function (resolve, reject) {
+            UserSchema.insertMany(parameters, function (error, res) {
+                if (!error) {
+                    resolve({ userName: res[0].userName, firstName: res[0].firstName, lastName: res[0].lastName });
+                } else {
+                    debuglog("ERROR", "UserSchema.insertMany", "error", "import Error: " + error.code);
+                    reject(error);
+                }
             });
+        });
+    }
+    async findOne(parameters) {
+        try {
+            const user = await UserSchema.findOne(parameters);
+            debuglog("DEBUG", "UserSchema.findOne", "user details", JSON.stringify(user));
+            return user;
         } catch (error) {
             debuglog("ERROR", "UserSchema.findOne", "error", error);
             throw error;
         }
-    }).catch(function (error) {
-        debuglog("ERROR", "UserSchema.findOne", "error", error);
-        throw error;
-    })
-}
-
-UserModel.prototype.update = function (conditions, update) {
-    var updateInfo = { $set: update }
-    return UserSchema.findOneAndUpdate(conditions, updateInfo).then(function (userInfo) {
-        return UserSchema.findOne(conditions);
-    }).catch(function (error) {
-        debuglog("ERROR", "UserSchema.findOneAndUpdate", "error", error);
-        throw error;
-    })
+    }
+    async login(parameters) {
+        try {
+            const infoUser = await UserSchema.findOne({ userName: parameters.userName });
+            debuglog("DEBUG", "UserSchema.findOne", "user details", JSON.stringify(infoUser));
+            if (!infoUser.isActive) {
+                return Promise.reject({ errorCode: ERROR_CODE.isACTIVE, message: "User is deactive." });
+            }
+            if (infoUser.password !== parameters.password) {
+                return Promise.reject({ errorCode: ERROR_CODE.WORNG_PASSWORD, message: "Password incorrect." });
+            }
+            var token = generateAuthToken(infoUser._id);
+            try {
+                return Promise.all([UserSchema.findOneAndUpdate({ _id: infoUser._id }, { $set: { token: token } })]).spread(function (res) {
+                    debuglog("DEBUG", "UserSchema.findOnAndUpdate", "respone", JSON.stringify(res));
+                    return UserSchema.findOne({ userName: parameters.userName });
+                });
+            } catch (error) {
+                debuglog("ERROR", "UserSchema.findOne", "error", error);
+                throw error;
+            }
+        } catch (error_1) {
+            debuglog("ERROR", "UserSchema.findOne", "error", error_1);
+            throw error_1;
+        }
+    }
+    async update(conditions, update) {
+        var updateInfo = { $set: update };
+        try {
+            const userInfo = await UserSchema.findOneAndUpdate(conditions, updateInfo);
+            return await UserSchema.findOne(conditions);
+        } catch (error) {
+            debuglog("ERROR", "UserSchema.findOneAndUpdate", "error", error);
+            throw error;
+        }
+    }
 }
 
 function generateAuthToken(params) {
